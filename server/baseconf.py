742db4f4bb727474545c52760f2db5ae726155ba
omid <omid7798@gmail.com>
2020-04-04 02:16:43 +0430

    update fetching host ip in config

--- server/baseconf.py
+++ server/baseconf.py
@@ -94,7 +94,7 @@ class Config:
         self.group_name = group.grp_name
 
     def calc_ip(self):
-        self.ip = socket.gethostbyname(socket.gethostname())
+        self.ip = [l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0]
     
     def dependencies_installation_check(self):
         if os.path.exists("/usr/bin/git") is True:
